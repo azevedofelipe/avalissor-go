a8af7d0fffc101b41956964e679d55b418a7d8fa
azevedofelipe <felipe.azevedo@dna.tec.br>
2025-06-07 00:39:51 -0300

    Create user creation API

--- main.go
+++ main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/azevedofelipe/avalissor-go/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
-	queries     *database.Queries
-	tokenSecret string
+	queries *database.Queries
 }
 
 func main() {
 	const port = "8080"
 
-	mux := http.NewServeMux()
+	godotenv.Load()
 
-	mux.HandleFunc("POST /api/login", apiCfg.handlerUserLogin)
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{
+		queries: dbQueries,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUserCreation)
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
---
+++ user_handler.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/azevedofelipe/avalissor-go/internal/database"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (cfg *apiConfig) handlerUserCreation(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+	}
+
+	params := parameters{}
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&params); err != nil {
+		log.Printf("Error reading json: %s", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	user, err := cfg.queries.CreateUser(r.Context(), database.CreateUserParams{
+		Email:    params.Email,
+		Username: params.Username,
+	})
+	if err != nil {
+		log.Printf("Error creating user: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	response := User{
+		ID:        user.ID,
+		Username:  user.Username,
+		Email:     user.Email,
+		UpdatedAt: user.UpdatedAt,
+		CreatedAt: user.CreatedAt,
+	}
+	dat, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("Error marshalling response json: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
+
+}
