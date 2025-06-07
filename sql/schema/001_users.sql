aaaab3b536c238cbf20f166b47a346311f8f7623
azevedofelipe <felipe.azevedo@dna.tec.br>
2025-06-06 23:35:10 -0300

    Create users table

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users(
+	id UUID PRIMARY KEY,
+	username TEXT NOT NULL UNIQUE,
+	email TEXT NOT NULL UNIQUE,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
